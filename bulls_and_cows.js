e75eae8203ea2789f4986949ded21696886fe3b6
Andrew <slonryba@gmail.com>
2020-03-04 17:07:59 +0300

    complete bc game

--- bulls_and_cows.js
+++ bulls_and_cows.js
@@ -1,18 +1,48 @@
 const readLine = require('readline-sync');
 
-const guessNumber = Math.floor(Math.random() * 1000) + Math.floor(Math.random() * 10000);
-const length = String(guessNumber).length;
+const secretNumber = Math.floor(Math.random() * 1000) + Math.floor(Math.random() * 100000);
+const length = String(secretNumber).length;
 
 console.log(`My guessed number contains ${length} digits`);
-console.log(guessNumber);
+console.log(secretNumber);
+
+const getHint = (secret, guess) => {
+
+let bulls = 0;
+let cows = 0;
+
+const secretArr = String(secret).split('').map(elem => +elem);
+const guessArr = guess.split('').map(elem => +elem);
+
+for (let i = 0; i < secretArr.length;) {
+    if (secretArr[i] === guessArr[i]) {
+        bulls += 1;
+        secretArr.splice(i, 1);
+        guessArr.splice(i, 1);
+    }
+    else i += 1;
+}
+
+for (let j = 0; j <= guessArr.length; j++) {
+    if (secretArr.includes(guessArr[j])) {
+        cows += 1;
+        secretArr.splice(secretArr.indexOf(guessArr[j]), 1);
+    }
+}
+
+    return `${bulls} bulls ${cows} cows`;
+};
 
 while (true) {
 
 const query = readLine.question("input your number: ");
-  if (+query === guessNumber) {
-    console.log("you win! number what i guess is really " + guessNumber);
+  if (+query === secretNumber) {
+    console.log("you win! number what i guess is really " + secretNumber);
     break;
   }
+
+console.log(`${getHint(secretNumber, query)}`);
+
 console.log(`return your ${query}`);
 
 };
