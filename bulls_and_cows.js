9d5484a400ac5b933a351910d3672b5c2503b066
Andrew <slonryba@gmail.com>
2020-03-06 16:43:10 +0300

    modified the bulls and cows file

--- bulls_and_cows.js
+++ bulls_and_cows.js
@@ -1,16 +1,15 @@
 const readLine = require('readline-sync');
-
+//generate the random number
 const secretNumber = Math.floor(Math.random() * 1000) + Math.floor(Math.random() * 100000);
 const length = String(secretNumber).length;
 
 console.log(`My guessed number contains ${length} digits`);
-console.log(secretNumber);
 
+//function compare the generated and input numbers
 const getHint = (secret, guess) => {
 
 let bulls = 0;
 let cows = 0;
-
 const secretArr = String(secret).split('').map(elem => +elem);
 const guessArr = guess.split('').map(elem => +elem);
 
@@ -29,20 +28,17 @@ for (let j = 0; j <= guessArr.length; j++) {
         secretArr.splice(secretArr.indexOf(guessArr[j]), 1);
     }
 }
-
-    return `${bulls} bulls ${cows} cows`;
+    return `in your number ${bulls} bulls and ${cows} cows`;
 };
 
+//start the game!
 while (true) {
 
 const query = readLine.question("input your number: ");
   if (+query === secretNumber) {
-    console.log("you win! number what i guess is really " + secretNumber);
+    console.log(`you win! number what i guess is really ${secretNumber}`);
     break;
   }
 
 console.log(`${getHint(secretNumber, query)}`);
-
-console.log(`return your ${query}`);
-
 };
