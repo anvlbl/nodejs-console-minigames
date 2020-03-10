1c5eb050fb4e7eb24ccd8dbe46c9e6ad8a169bbf
Andrew <slonryba@gmail.com>
2020-03-10 15:13:05 +0300

    add new functions

--- rpg_battle.js
+++ rpg_battle.js
@@ -1,65 +1,104 @@
 const monster = {
     maxHealth: 10,
+	currentHealth: 10,
     name: "Лютый",
     moves: [
         {
             "name": "Удар когтистой лапой",
-            "physicalDmg": 3, // физический урон
-            "magicDmg": 0,    // магический урон
-            "physicArmorPercents": 20, // физическая броня
-            "magicArmorPercents": 20,  // магическая броня
-            "cooldown": 0     // ходов на восстановление
+            "physicalDmg": 3, 
+            "magicDmg": 0,    
+            "physicArmor%": 20, 
+            "magicArmor%": 20,  
+            "cooldown": 0,     
+			"currentCooldown": 0,
         },
         {
             "name": "Огненное дыхание",
             "physicalDmg": 0,
             "magicDmg": 4,
-            "physicArmorPercents": 0,
-            "magicArmorPercents": 0,
-            "cooldown": 3
+            "physicArmor%": 0,
+            "magicArmor%": 0,
+            "cooldown": 3,
+			"currentCooldown": 0,
         },
         {
             "name": "Удар хвостом",
             "physicalDmg": 2,
             "magicDmg": 0,
-            "physicArmorPercents": 50,
-            "magicArmorPercents": 0,
-            "cooldown": 2
+            "physicArmor%": 50,
+            "magicArmor%": 0,
+            "cooldown": 2,
+			"currentCooldown": 0,
         },
     ]
 };
 
-moves: [
+const character = {
+	maxHealth: 10,
+	currentHealth: 10,
+	name: "Евстафий",
+	moves: [
     {
         "name": "Удар боевым кадилом",
         "physicalDmg": 2,
         "magicDmg": 0,
-        "physicArmorPercents": 0,
-        "magicArmorPercents": 50,
-        "cooldown": 0
+        "physicArmor%": 0,
+        "magicArmor%": 50,
+        "cooldown": 0,
+		"currentCooldown": 0,
     },
     {
         "name": "Вертушка левой пяткой",
         "physicalDmg": 4,
         "magicDmg": 0,
-        "physicArmorPercents": 0,
-        "magicArmorPercents": 0,
-        "cooldown": 4
+        "physicArmor%": 0,
+        "magicArmor%": 0,
+        "cooldown": 4,
+		"currentCooldown": 0,
     },
     {
         "name": "Каноничный фаербол",
         "physicalDmg": 0,
         "magicDmg": 5,
-        "physicArmorPercents": 0,
-        "magicArmorPercents": 0,
-        "cooldown": 3
+        "physicArmor%": 0,
+        "magicArmor%": 0,
+        "cooldown": 3,
+		"currentCooldown": 0,
     },
     {
         "name": "Магический блок",
         "physicalDmg": 0,
         "magicDmg": 0,
-        "physicArmorPercents": 100,
-        "magicArmorPercents": 100,
-        "cooldown": 4
+        "physicArmor%": 100,
+        "magicArmor%": 100,
+        "cooldown": 4,
+		"currentCooldown": 0,
     },
 ]
+};
+
+
+
+const hit = obj => {	
+	//choose the hit with max damage (physical or magic)
+	const changeMaxDmg = arr => {
+		let dmg = 0, index = 0;
+		// check the everything values of damage below
+		// at first physical damage
+		for (let i = 0; i < arr.length; i++) {
+			if ((arr[i].physicalDmg > dmg) && (arr[i].currentCooldown === 0)) {				
+				index = i;
+			}
+		}
+		//and magic damage
+		for (let i = 0; i < arr.length; i++) {
+			if ((arr[i].magicDmg > dmg) && (arr[i].currentCooldown === 0)) {				
+				index = i;
+			}
+		}
+		return arr[index];
+	}	
+	console.dir(changeMaxDmg(obj.moves));
+};
+
+hit(character);
