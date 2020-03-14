e5f7804b784cc8e608cbe11fafcbb6c21dad9bbf
Andrew <slonryba@gmail.com>
2020-03-14 20:59:51 +0300

    write some working functions + logic

--- rpg_battle/rpg_battle.js
+++ rpg_battle/rpg_battle.js
@@ -1,37 +1,38 @@
 const readline = require('readline-sync');
+
 const monster = {
     maxHealth: 30,
 	currentHealth: 30,
     name: "Лютый",
     moves: [
-        {   "id": "1",
-            "name": "Удар когтистой лапой",
-            "physicalDmg": 3, 
-            "magicDmg": 0,    
-            "physicArmor%": 20, 
-            "magicArmor%": 20,  
-            "CD": 0,        // cooldown
-			"currCD": 0,    // current cooldown
-        },
-        {   "id": "2",
-            "name": "Огненное дыхание",
-            "physicalDmg": 0,
-            "magicDmg": 4,
-            "physicArmor%": 0,
-            "magicArmor%": 0,
-            "CD": 3,
-			"currCD": 0,
-        },
-        {   "id": "3",
-            "name": "Удар хвостом",
-            "physicalDmg": 2,
-            "magicDmg": 0,
-            "physicArmor%": 50,
-            "magicArmor%": 0,
-            "CD": 2,
-			"currCD": 0,
-        },
-    ]
+    {   "id": "1",
+        "name": "Удар когтистой лапой",
+        "physicalDmg": 3, 
+        "magicDmg": 0,    
+        "physicArmorPercent": 20, 
+        "magicArmorPercent": 20,  
+        "CD": 0,        // cooldown
+        "currCD": 0,    // current cooldown
+    },
+    {   "id": "2",
+        "name": "Огненное дыхание",
+        "physicalDmg": 0,
+        "magicDmg": 4,
+        "physicArmorPercent": 0,
+        "magicArmorPercent": 0,
+        "CD": 3,
+        "currCD": 0,
+    },
+    {   "id": "3",
+        "name": "Удар хвостом",
+        "physicalDmg": 2,
+        "magicDmg": 0,
+        "physicArmorPercent": 50,
+        "magicArmorPercent": 0,
+        "CD": 2,
+        "currCD": 0,
+    },
+]
 };
 
 const character = {
@@ -43,89 +44,54 @@ const character = {
         "name": "Удар боевым кадилом",
         "physicalDmg": 2,
         "magicDmg": 0,
-        "physicArmor%": 0,
-        "magicArmor%": 50,
+        "physicArmorPercent": 0,
+        "magicArmorPercent": 50,
         "CD": 0,
-		"currCD": 0,
+        "currCD": 0,
     },
     {   "id": "2",
         "name": "Вертушка левой пяткой",
         "physicalDmg": 4,
         "magicDmg": 0,
-        "physicArmor%": 0,
-        "magicArmor%": 0,
+        "physicArmorPercent": 0,
+        "magicArmorPercent": 0,
         "CD": 4,
-		"currCD": 0,
+        "currCD": 0,
     },
     {   "id": "3",
         "name": "Каноничный фаербол",
         "physicalDmg": 0,
         "magicDmg": 5,
-        "physicArmor%": 0,
-        "magicArmor%": 0,
+        "physicArmorPercent": 0,
+        "magicArmorPercent": 0,
         "CD": 3,
-		"currCD": 0,
+        "currCD": 0,
     },
     {   "id": "4",
         "name": "Магический блок",
         "physicalDmg": 0,
         "magicDmg": 0,
-        "physicArmor%": 100,
-        "magicArmor%": 100,
+        "physicArmorPercent": 100,
+        "magicArmorPercent": 100,
         "CD": 4,
-		"currCD": 0,
+        "currCD": 0,
     },
 ]
 };
 
-// //function describes only monster actions logic - inflict max damage!
-// const hit = obj => {	
-// 	//choose the move with max damage (physical or magic)
-// 	const chooseMaxDmg = arr => {
-// 		const len = arr.length;
-// 		let dmg = 0, index = 0;
-// 		// check the everything values of damage and CD below
-// 		for (let i = 0; i < len; i++) {
-// 			if ((arr[i].physicalDmg > dmg) && (arr[i].currCD > 0)){ 
-// 				arr[i].currCD -= 1;
-// 			}
-// 			if ((arr[i].physicalDmg > dmg) && (arr[i].currCD === 0)) {	
-// 				dmg = arr[i].physicalDmg;
-// 				index = i;
-// 			}
-// 		}
-// 		for (let i = 0; i < len; i++) {
-// 			if ((arr[i].magicDmg > dmg) && (arr[i].currCD > 0)){
-// 				arr[i].currCD -= 1;
-// 			}
-// 			if ((arr[i].magicDmg > dmg) && (arr[i].currCD === 0)) {	
-// 				dmg = arr[i].magicDmg;			
-// 				index = i;
-// 			}
-// 		}		
-// 		arr[index].currCD = arr[index].CD;
-				
-// 		console.log(`you are take the ${dmg} damage!`);
-// 		return dmg;
-// 	}	
-// 	return chooseMaxDmg(obj.moves);	
-// };
-
-//function below describes monster actions logic - inflict max damage!
+//function describes monster actions logic
 const hit = obj => {	
 	//choose the move with max damage (physical or magic)
 	const action = arr => {
 		const len = arr.length;
-		let dmg = 0, index = 0; dmgType = ""
-		// check the everything values of damage and CD below
+		let dmg = 0, index = 0;
 		for (let i = 0; i < len; i++) {
 			if ((arr[i].physicalDmg > dmg) && (arr[i].currCD > 0)){ 
 				arr[i].currCD -= 1;
 			}
 			if ((arr[i].physicalDmg > dmg) && (arr[i].currCD === 0)) {	
 				dmg = arr[i].physicalDmg;
-                index = i;
-                damageType = "physical";
+                index = i;                
 			}
 		}
 		for (let i = 0; i < len; i++) {
@@ -134,19 +100,20 @@ const hit = obj => {
 			}
 			if ((arr[i].magicDmg > dmg) && (arr[i].currCD === 0)) {	
 				dmg = arr[i].magicDmg;			
-                index = i;
-                damageType = "magic";
+                index = i;               
 			}
 		}		
-		arr[index].currCD = arr[index].CD;
-		
-		return {
-            name: arr[index].name,
-            damage: dmg,
-            damageType: dmgType
-        };
+		arr[index].currCD = arr[index].CD;		
+		return arr[index];        
 	}	
-	return action(obj.moves);	
+	return action(obj.moves);	//return the object (one of the action list)
+};
+
+//function below calculates the applied damage
+const diff = (minuend, subtrahend) => {
+    const physDmg = subtrahend.physicalDmg - (minuend.physicArmorPercent / 100 * subtrahend.physicalDmg);
+    const magDmg = subtrahend.magicDmg - (minuend.magicArmorPercent / 100 * subtrahend.magicDmg);
+    return physDmg + magDmg;
 };
 
 while (true) {
@@ -158,47 +125,15 @@ while (true) {
         console.log('you lose!');
         break;
     }
-    console.log(`current health of enemy: ${monster.currentHealth}`);
-    console.log(`your current health: ${character.currentHealth}`);
-    console.table(character.moves);
+    // show current health of opponents
+    console.log(`enemy's health: ${monster.currentHealth}`);
+    console.log(`your health: ${character.currentHealth}`);
 
-    let monsterMove = hit(monster);
-    console.log(`monster says: "Now i will cast ${monsterMove.name}"`)
-    const move = readline.question('What do we do?');	
-    monster.currentHealth -= hit(character);
-    
+    const mdm = hit(monster);
+    const cdm = hit(character);
+    console.log(`${monster.name} applies the ${mdm.name}`);
+    console.log(`${character.name} applies the ${cdm.name}`);
+
+    monster.currentHealth -= diff(mdm, cdm);
+    character.currentHealth -=diff(cdm, mdm);
 };
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
