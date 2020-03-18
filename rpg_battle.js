44c8d243f909109e78d6d45c0d0be5f5508391e2
Andrew <slonryba@gmail.com>
2020-03-18 18:41:35 +0300

    add cooldown counter

--- rpg_battle.js
+++ rpg_battle.js
@@ -75,44 +75,47 @@ const character = {
 
 //function describes monster actions logic
 //return the object (one of the action list with max damage and zero coooldown)
-//choose the move with max damage (physical or magic)
-	const hit = arr => {
-		const len = arr.length;
-		let dmg = 0, index = 0;
-		for (let i = 0; i < len; i++) {
-			if ((arr[i].physicalDmg > dmg) && (arr[i].currCD > 0)){ 
-				arr[i].currCD -= 1;
-			}
-			if ((arr[i].physicalDmg > dmg) && (arr[i].currCD === 0)) {	
-				dmg = arr[i].physicalDmg;
-                index = i;                
-			}
+//choose the action with max damage (physical or magic)
+const hit = arr => {
+	const len = arr.length;
+	let dmg = 0, index = 0;
+	for (let i = 0; i < len; i++) {
+		if ((arr[i].physicalDmg > dmg) && (arr[i].currCD > 0)){ 
+			arr[i].currCD -= 1;
 		}
-		for (let i = 0; i < len; i++) {
-			if ((arr[i].magicDmg > dmg) && (arr[i].currCD > 0)){
-				arr[i].currCD -= 1;
-			}
-			if ((arr[i].magicDmg > dmg) && (arr[i].currCD === 0)) {	
-				dmg = arr[i].magicDmg;			
-                index = i;               
-			}
-		}		
-		arr[index].currCD = arr[index].CD;		
-		return arr[index];        
-	};
+		if ((arr[i].physicalDmg > dmg) && (arr[i].currCD === 0)) {	
+			dmg = arr[i].physicalDmg;
+            index = i;                
+		}
+	}
+	for (let i = 0; i < len; i++) {
+		if ((arr[i].magicDmg > dmg) && (arr[i].currCD > 0)){
+			arr[i].currCD -= 1;
+		}
+		if ((arr[i].magicDmg > dmg) && (arr[i].currCD === 0)) {	
+			dmg = arr[i].magicDmg;			
+            index = i;               
+		}
+	}		
+	arr[index].currCD = arr[index].CD;		
+	return arr[index];        
+};
 
 //function below calculates the applied damage
 const diff = (minuend, subtrahend) => {
 	if ((typeof(minuend) !== "object") || (typeof(subtrahend) !== "object")) {		
-		return 0;
+		return -1;
 	}
     const physDmg = subtrahend.physicalDmg - (minuend.physicArmorPercent / 100 * subtrahend.physicalDmg);
     const magDmg = subtrahend.magicDmg - (minuend.magicArmorPercent / 100 * subtrahend.magicDmg);
-    return physDmg + magDmg;
+    return Math.round(physDmg + magDmg);
 };
 
-
+console.table(monster.moves);
 while (true) {
+	if ((monster.currentHealth <= 0) && (character.currentHealth <= 0)) {
+		console.log();
+	}
 	if (monster.currentHealth <= 0) {
 		console.log('you won!');
 		break;
@@ -131,19 +134,27 @@ while (true) {
 	console.table(character.moves); 	
 	console.log(`${monster.name} says: "Now i apply ${mdm.name}"`);
 	
-	const playerInput = readline.question('what do we do?')
+	const playerInput = readline.question('what do we do? ')
 
-    //calculate the damage of player
+    //requesting an action from the player and cheking the input
     const cdm = character.moves[+playerInput];
-
-	if ((diff(mdm, cdm) === 0) || diff(cdm, mdm) === 0) {
+	if (cdm.currCD !== 0) {
+		console.log(`this action is not ready`);
+		continue;
+	}
+	if ((diff(mdm, cdm) === -1) || (diff(cdm, mdm) === -1)) {
 		console.log("incorrect input data");
 		continue;
 	}
+	
+	cdm.currCD = cdm.CD;
+	
 	monster.currentHealth -= diff(mdm, cdm);
     character.currentHealth -=diff(cdm, mdm);
 
     console.log(`${monster.name} applies the ${mdm.name} and give ${mdm.physicalDmg + mdm.magicDmg} damage`);
     console.log(`${character.name} applies the ${cdm.name} and give ${cdm.physicalDmg + cdm.magicDmg} damage`);    
-		
+	
+	monster.moves.map(elem => elem.currCD <= 0 ? elem.currCD = 0 : elem.currCD -= 1);
+	character.moves.map(elem => elem.currCD <= 0 ? elem.currCD = 0 : elem.currCD -= 1);
 };
