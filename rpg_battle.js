85be72af5971729b0b93223e06ed7fcc83d40ad0
Andrew <slonryba@gmail.com>
2020-03-18 13:37:13 +0300

    delete unnecessary

--- rpg_battle.js
+++ rpg_battle.js
@@ -1,3 +1,4 @@
+"use strict"
 const readline = require('readline-sync');
 
 const monster = {
@@ -5,164 +6,144 @@ const monster = {
 	currentHealth: 30,
     name: "Лютый",
     moves: [
-        {
-			"id": 1,
-            "name": "Удар когтистой лапой",
-            "physicalDmg": 3, 
-            "magicDmg": 0,    
-            "physicArmor%": 20, 
-            "magicArmor%": 20,  
-            "cooldown": 0,     
-			"currentCooldown": 0,
-        },
-        {
-			"id": 2,
-            "name": "Огненное дыхание",
-            "physicalDmg": 0,
-            "magicDmg": 4,
-            "physicArmor%": 0,
-            "magicArmor%": 0,
-            "cooldown": 3,
-			"currentCooldown": 0,
-        },
-        {
-			"id": 3,
-            "name": "Удар хвостом",
-            "physicalDmg": 2,
-            "magicDmg": 0,
-            "physicArmor%": 50,
-            "magicArmor%": 0,
-            "cooldown": 2,
-			"currentCooldown": 0,
-        },
-    ]
-};
-
-const character = {
-	maxHealth: 10,
-	currentHealth: 10,
-	name: "Евстафий",
-	moves: [
-    {
-		"id": 1,
-        "name": "Удар боевым кадилом",
+    {   "name": "Удар когтистой лапой",
+        "physicalDmg": 3, 
+        "magicDmg": 0,    
+        "physicArmorPercent": 20, 
+        "magicArmorPercent": 20,  
+        "CD": 0,        // cooldown
+        "currCD": 0,    // current cooldown
+    },
+    {   "name": "Огненное дыхание",
+        "physicalDmg": 0,
+        "magicDmg": 4,
+        "physicArmorPercent": 0,
+        "magicArmorPercent": 0,
+        "CD": 3,
+        "currCD": 0,
+    },
+    {   "name": "Удар хвостом",
         "physicalDmg": 2,
         "magicDmg": 0,
-        "physicArmor%": 0,
-        "magicArmor%": 50,
-        "cooldown": 0,
-		"currentCooldown": 0,
-    },
-    {
-		"id": 2,
-        "name": "Вертушка левой пяткой",
-        "physicalDmg": 4,
-        "magicDmg": 0,
-        "physicArmor%": 0,
-        "magicArmor%": 0,
-        "cooldown": 4,
-		"currentCooldown": 0,
-    },
-    {
-		"id": 3,
-        "name": "Каноничный фаербол",
-        "physicalDmg": 0,
-        "magicDmg": 5,
-        "physicArmor%": 0,
-        "magicArmor%": 0,
-        "cooldown": 3,
-		"currentCooldown": 0,
-    },
-    {
-		"id": 4,
-        "name": "Магический блок",
-        "physicalDmg": 0,
-        "magicDmg": 0,
-        "physicArmor%": 100,
-        "magicArmor%": 100,
-        "cooldown": 4,
-		"currentCooldown": 0,
+        "physicArmorPercent": 50,
+        "magicArmorPercent": 0,
+        "CD": 2,
+        "currCD": 0,
     },
 ]
 };
 
+const character = {
+	maxHealth: 30,
+	currentHealth: 30,
+	name: "Евстафий",
+	moves: [
+    {   "name": "Удар боевым кадилом",
+        "physicalDmg": 2,
+        "magicDmg": 0,
+        "physicArmorPercent": 0,
+        "magicArmorPercent": 50,
+        "CD": 0,
+        "currCD": 0,
+    },
+    {   "name": "Вертушка левой пяткой",
+        "physicalDmg": 4,
+        "magicDmg": 0,
+        "physicArmorPercent": 0,
+        "magicArmorPercent": 0,
+        "CD": 4,
+        "currCD": 0,
+    },
+    {   "name": "Каноничный фаербол",
+        "physicalDmg": 0,
+        "magicDmg": 5,
+        "physicArmorPercent": 0,
+        "magicArmorPercent": 0,
+        "CD": 3,
+        "currCD": 0,
+    },
+    {   "name": "Магический блок",
+        "physicalDmg": 0,
+        "magicDmg": 0,
+        "physicArmorPercent": 100,
+        "magicArmorPercent": 100,
+        "CD": 4,
+        "currCD": 0,
+    },
+]
+};
 
-
-const hit = obj => {	
-	//choose the move with max damage (physical or magic)
-	const choiseMaxDmg = arr => {
+//function describes monster actions logic
+//return the object (one of the action list with max damage and zero coooldown)
+//choose the move with max damage (physical or magic)
+	const hit = arr => {
 		const len = arr.length;
-		let dmg = 0, index = 0; typeOfDamage = "";
-		// check the everything values of damage and cooldown below
+		let dmg = 0, index = 0;
 		for (let i = 0; i < len; i++) {
-			if ((arr[i].physicalDmg > dmg) && (arr[i].currentCooldown > 0)){ 
-				arr[i].currentCooldown -= 1;
+			if ((arr[i].physicalDmg > dmg) && (arr[i].currCD > 0)){ 
+				arr[i].currCD -= 1;
 			}
-			if ((arr[i].physicalDmg > dmg) && (arr[i].currentCooldown === 0)) {	
+			if ((arr[i].physicalDmg > dmg) && (arr[i].currCD === 0)) {	
 				dmg = arr[i].physicalDmg;
-				index = i;
-				typeOfDamage = "physical";
+                index = i;                
 			}
 		}
 		for (let i = 0; i < len; i++) {
-			if ((arr[i].magicDmg > dmg) && (arr[i].currentCooldown > 0)){
-				arr[i].currentCooldown -= 1;
+			if ((arr[i].magicDmg > dmg) && (arr[i].currCD > 0)){
+				arr[i].currCD -= 1;
 			}
-			if ((arr[i].magicDmg > dmg) && (arr[i].currentCooldown === 0)) {	
+			if ((arr[i].magicDmg > dmg) && (arr[i].currCD === 0)) {	
 				dmg = arr[i].magicDmg;			
-				index = i;
-				typeOfDamage = "magic";
+                index = i;               
 			}
 		}		
-		arr[index].currentCooldown = arr[index].cooldown;
-				
-		console.log(`you are take the ${dmg} damage!`);
-		return dmg;
-	}	
-	return choiseMaxDmg(obj.moves);	
+		arr[index].currCD = arr[index].CD;		
+		return arr[index];        
+	};
+
+//function below calculates the applied damage
+const diff = (minuend, subtrahend) => {
+	if ((typeof(minuend) !== "object") || (typeof(subtrahend) !== "object")) {		
+		return 0;
+	}
+    const physDmg = subtrahend.physicalDmg - (minuend.physicArmorPercent / 100 * subtrahend.physicalDmg);
+    const magDmg = subtrahend.magicDmg - (minuend.magicArmorPercent / 100 * subtrahend.magicDmg);
+    return physDmg + magDmg;
 };
+
 
 while (true) {
 	if (monster.currentHealth <= 0) {
 		console.log('you won!');
 		break;
-	}	
-	console.log('current health of enemy ' + monster.currentHealth);	
-	console.table(character.moves);
-	const query = readline.question(`Удар?`);
-	monster.currentHealth -= hit(character);
+    }	    
+    if (character.currentHealth <= 0) {
+        console.log('you lose!');
+        break;
+    }
+    // show current health of opponents
+    console.log(`health of ${monster.name}: ${monster.currentHealth}`);
+    console.log(`health of ${character.name}: ${character.currentHealth}`);
+
+	// calculate the action of monster
+	const mdm = hit(monster.moves);
+
+	console.table(character.moves); 	
+	console.log(`${monster.name} says: "Now i apply ${mdm.name}"`);
+	
+	const playerInput = readline.question('what do we do?')
+
+    //calculate the damage of player
+    const cdm = character.moves[+playerInput];
+
+	if ((diff(mdm, cdm) === 0) || diff(cdm, mdm) === 0) {
+		console.log("incorrect input data");
+		continue;
+	}
+	monster.currentHealth -= diff(mdm, cdm);
+    character.currentHealth -=diff(cdm, mdm);
+
+    console.log(`${monster.name} applies the ${mdm.name} and give ${mdm.physicalDmg + mdm.magicDmg} damage`);
+    console.log(`${character.name} applies the ${cdm.name} and give ${cdm.physicalDmg + cdm.magicDmg} damage`);    
+		
 };
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
